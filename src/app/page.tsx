617d0e08aa1255e5f3e8b29b5af150c8197cfc07
Kevin Roy <kevnroy1@gmail.com>
2025-05-14 23:36:22 +0530

    chore: resolving build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p>yo this is kevz, and you're tuning into <span className="text-blue-500">digital station.</span></p>
+        <p>yo this is kevz, and you&apos;re tuning into <span className="text-blue-500">digital station.</span></p>
       </main>
     </div>
   );
